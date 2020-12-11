94afd469c259b95c47c31c4fef7a212aef16459b
Mathieu Margotin <mathieu.margotin@gmail.com>
2020-12-11 18:49:34 +0100

    Section2-9

---
+++ readme.md
@@ -0,0 +1,6 @@
+**PHPUnit** in 2020 - Unit & **Selenium** Testing For PHP Developers   
+###### Includes **Test-Driven Development (TDD)** by creating real life app with PHP & PHPUnit and Selenium acceptance testing   
+
+---
+
+<https://www.udemy.com/course/phpunit-unit-selenium-testing-for-php-developers/>
---
+++ tests/BMICalculatorTest.php
@@ -0,0 +1,47 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class BMICalculatorTest extends TestCase
+{
+    public function test_underweight_BMI_text_result()
+    {
+        $BMICalculator = new BMICalculator;
+        $BMICalculator->BMI = 10;
+        $result = $BMICalculator->getTextResultFromBMI();
+
+        $expected = 'Underweight';
+        $this->assertSame($expected, $result);
+    }
+
+    public function test_normal_BMI_text_result()
+    {
+        $BMICalculator = new BMICalculator;
+        $BMICalculator->BMI = 24;
+        $result = $BMICalculator->getTextResultFromBMI();
+
+        $expected = 'Normal';
+        $this->assertSame($expected, $result);
+    }
+
+    public function test_overweight_BMI_text_result()
+    {
+        $BMICalculator = new BMICalculator;
+        $BMICalculator->BMI = 28;
+        $result = $BMICalculator->getTextResultFromBMI();
+
+        $expected = 'Overweight';
+        $this->assertSame($expected, $result);
+    }
+
+    public function test_correct_BMI_value()
+    {
+        $BMICalculator = new BMICalculator;
+        $BMICalculator->mass = 100; // kg
+        $BMICalculator->height = 1.6; // m
+        $result = $BMICalculator->calculate();
+
+        $expected = 39.1;
+        $this->assertSame($expected, $result);
+    }
+}
